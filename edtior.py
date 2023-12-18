151f782216b2f49c6020ba13ca5b69a1bc370d89
Kadi17 <kapmid123@gmail.com>
2023-12-18 20:38:08 +0100

    add finish python script

--- edtior.py
+++ edtior.py
@@ -9,6 +9,29 @@ for file in files:
         posts.append(file)
 
 # print(posts)
+lines = []
+linesToDelete =[]
+# read file
+with open(r"index.html", 'r') as fp:
+    # read an store all lines into list
+    lines = fp.readlines()
+    for idx, line in enumerate(lines, start=1):
+        print(line)
+        if re.search("<!-- StartContent -->", line):
+            linesToDelete.append(idx)
+            print(idx)
+        if re.search("<!-- EndContent -->", line):
+            linesToDelete.append(idx)
+            
+# Write file
+with open(r"index.html", 'w') as fp:
+    # iterate each line
+    for number, line in enumerate(lines):
+        # delete line 5 and 8. or pass any Nth line you want to remove
+        # note list index starts from 0
+        if number not in range(linesToDelete[0], (linesToDelete[1]-1)):
+            fp.write(line)
+
 
 for post in posts:
     evenN = 1
@@ -23,25 +46,16 @@ for post in posts:
             cardContent = cardContent + ' ' + message
         if message == '<!--start-->':
             evenN = evenN + 1
-            tablica.append(cardContent)
+            if cardContent != '':
+                tablica.append(cardContent)
             cardContent =''
     
-    # index = open('test2.html', 'r+')
-    # indexContent = index.read()
-    # indexWords = re.split(" ", indexContent)
-    # for message in indexWords:
-    #     if evenN % 2 == 0 and message != '<!--content-->':
-    #         print('widzę plik')
-    #         index.write("Działa")
-    #     if message == '<!--content-->':
-    #         evenN = evenN + 1
-
-    with open("test2.html", "r") as f:
+    with open("index.html", "r") as f:
         contents = f.readlines()
 
-    contents.insert(9, '<div class="panel" class="blog_layouts_post_card blog_layouts_shadow"> <h3>'+ tablica[1] +'</h3> <p>12-12-2003 </p> <p class="zajawka">'+ tablica[3] +'</p><a href="Post.html"><button class="button">Read More</button></a></div>')
+    contents.insert(linesToDelete[0], '<div class="panel" class="blog_layouts_post_card blog_layouts_shadow"> <h3>'+ tablica[1] +'</h3> <p>12-12-2003 </p> <p class="zajawka">'+ tablica[0] +'</p><a href="'+post+'"><button class="button">Read More</button></a></div>'+'\n')
 
-    with open("test2.html", "w") as f:
+    with open("index.html", "w") as f:
         contents = "".join(contents)
         f.write(contents)
     print(tablica)
